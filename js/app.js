75115d365706155996cf8f089630e28203b4c773
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2025-01-20 22:09:55 +0000

    Adds set limit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 class CalorieTracker {
 	constructor() {
-		this._calorieLimit = 2000
+		this._calorieLimit = Storage.getCalorieLimit()
 		this._totalCalories = 0
 		this._meals = []
 		this._workouts = []
@@ -53,6 +53,13 @@ class CalorieTracker {
 		this._totalCalories = 0
 		this._meals = []
 		this._workouts = []
+		this._render()
+	}
+
+	setLimit(calorieLimit) {
+		this._calorieLimit = calorieLimit
+		Storage.setCalorieLimit(calorieLimit)
+		this._displayCaloriesLimit()
 		this._render()
 	}
 
@@ -191,6 +198,22 @@ class Workout {
 	}
 }
 
+class Storage {
+	static getCalorieLimit(defaultLimit = 2000) {
+		let calorieLimit
+		if (localStorage.getItem('calorieLimit') === null) {
+			calorieLimit = defaultLimit
+		} else {
+			calorieLimit = +localStorage.getItem('calorieLimit')
+		}
+		return calorieLimit
+	}
+
+	static setCalorieLimit(calorieLimit) {
+		localStorage.setItem('calorieLimit', calorieLimit)
+	}
+}
+
 class App {
 	constructor() {
 		this._tracker = new CalorieTracker()
@@ -216,6 +239,9 @@ class App {
 		document
 			.getElementById('reset')
 			.addEventListener('click', this._reset.bind(this))
+		document
+			.getElementById('limit-form')
+			.addEventListener('submit', this._setLimit.bind(this))
 	}
 
 	_newItem(type, e) {
@@ -282,6 +308,24 @@ class App {
 		document.getElementById('workout-items').innerHTML = ''
 		document.getElementById('filter-meals').value = ''
 		document.getElementById('filter-workouts').value = ''
+	}
+
+	_setLimit(e) {
+		e.preventDefault()
+
+		const limit = document.getElementById('limit')
+
+		if (limit.value === '') {
+			alert('Please add a limit')
+			return
+		}
+
+		this._tracker.setLimit(+limit.value)
+		limit.value = ''
+
+		const modalEl = document.getElementById('limit-modal')
+		const modal = bootstrap.Modal.getInstance(modalEl)
+		modal.hide()
 	}
 }
 
